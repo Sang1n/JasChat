c796500f28ac10fa694c25216e9f0e92a382ae03
Компьютер <@DESKTOP-2GHAPQU>
2024-06-27 18:42:45 +0300

    Add project files.

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Chat.Models;
+
+namespace Chat.Hubs;
+
+public interface IChatClient
+{
+    public Task ReceiveMessage(string userName, string message);
+}
+
+
+
+public class ChatHub : Hub<IChatClient>
+{
+    public async Task JoinChat(UserConnection connection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+        await Clients
+            .Group(connection.ChatRoom)
+            .ReceiveMessage("Admin", $"{connection.UserName} присоединился к чату");
+    }
+}
+
+
+
